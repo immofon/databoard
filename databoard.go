1cf8005c509bf23575fe29da1c6ea2277aa1a637
mofon <immofon@163.com>
2017-04-11 16:45:36 +0800

    add Databoard.GetReleases()

--- databoard.go
+++ databoard.go
@@ -37,3 +37,36 @@ func (d *Databoard) GetLatestRelease(ctx context.Context) (*github.RepositoryRel
 	ret, _, err := d.c.Repositories.GetLatestRelease(ctx, d.Owner, d.Repo)
 	return ret, err
 }
+
+func (d *Databoard) GetReleases(ctx context.Context) (<-chan *github.RepositoryRelease, error) {
+	ch := make(chan *github.RepositoryRelease)
+
+	go func(ch chan<- *github.RepositoryRelease) {
+		defer close(ch)
+		listOptions := &github.ListOptions{
+			Page:    1,
+			PerPage: 1,
+		}
+		for {
+			releases, resp, err := d.c.Repositories.ListReleases(ctx, d.Owner, d.Repo, listOptions)
+			if err != nil {
+				return
+			}
+
+			for _, rel := range releases {
+				select {
+				case <-ctx.Done():
+					return
+				case ch <- rel:
+				}
+			}
+
+			if resp.NextPage == 0 {
+				return
+			}
+
+			listOptions.Page = resp.NextPage
+		}
+	}(ch)
+	return ch, nil
+}
